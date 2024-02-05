0ba6d68c644b1f119752188430d425bb1110774a
ash <ashchander@gmail.com>
2024-02-05 16:31:10 -0500

    csv parsing

--- ex1/main.go
+++ ex1/main.go
@@ -20,14 +20,34 @@ func checkFlags() Settings {
 	}
 }
 
+func getCsvReader(filename string) (*csv.Reader, *os.File) {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Could not open file, %s", filename))
+	}
+	// defer file.Close()
+
+
+	csvReader := csv.NewReader(file)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Could not read file, %s", filename))
+	}
+	return csvReader, file
+}
+
 func main() {
 	settings := checkFlags()
-	data, err := os.ReadFile(settings.file)
-	if err != nil {
-		log.Fatal(fmt.Sprintf("Could not read file, %s", settings.file))
+	csvReader, file := getCsvReader(settings.file)
+	defer file.Close()
+
+	for problem, err := csvReader.Read(); problem != nil; {
+		if err != nil {
+			log.Fatal(fmt.Sprintf("Could not parse csv, %s", settings.file))
+		}
+
+
+		fmt.Printf("%s, answer is: %s\n", problem[0], problem[1])
+		problem, err = csvReader.Read()
 	}
 
-	csvReader := csv.NewReader(data)
-
-	os.Stdout.Write(data)
 }
